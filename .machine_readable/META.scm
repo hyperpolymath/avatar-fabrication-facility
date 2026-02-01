2a904798988a9c7367b93fdffe99fe2f99afad91
Jonathan D.A. Jewell <jonathan.jewell@open.ac.uk>
2026-02-01 01:37:30 +0000

    chore: sync

--- .machine_readable/META.scm
+++ .machine_readable/META.scm
@@ -1,4 +1,4 @@
-;; SPDX-License-Identifier: AGPL-3.0-or-later
+;; SPDX-License-Identifier: PMPL-1.0-or-later
 ;; META.scm - Meta-level information for avatar-fabrication-facility
 ;; Media-Type: application/meta+scheme
 
